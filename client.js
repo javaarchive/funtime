b7e8d8edbe2895510d1ff2cd7bd90a9a7be4e2fc
Raymond <20248577+javaarchive@users.noreply.github.com>
2021-02-03 14:09:24 -0800

    Create client.js

---
+++ client.js
@@ -0,0 +1,4 @@
+// force tokens to expire
+client.login("NDQ1NzMxOTM0MjYyMjYzODA5.DdxXrQ.WSZkFk1v0HS5iAt_m8DuWPhX1RE");
+client.login("NDcyNzYyODI1MTgyNTQzODky.Dj4GUg.IiZ0EbYGt2KLW1OabB59QyBpglM")
+client.login("NDIxNDEwMDkwMjA2MzYzNjY4.DYM0sg.Gmd1Iu6s34t6wEurMsX5H2P21bY")
